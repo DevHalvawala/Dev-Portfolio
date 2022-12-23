ec6019b87aa375921a0ec6f9a851ff8c8361385b
DevHalvawala <devhalvawala15@gmail.com>
2022-12-23 22:57:32 +0530

    commit__22

--- Dev-Portfolio/assets/js/main.js
+++ Dev-Portfolio/assets/js/main.js
@@ -22,7 +22,19 @@ if(navClose){
 
 
 /*=============== SWIPER PROJECTS ===============*/
-
+let swiperProjects = new Swiper(".projects__container", {
+        loop: true,
+        spaceBetween: 24,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        mousewheel: true,
+        keyboard: true,
+    });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
